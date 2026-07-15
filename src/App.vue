dc22f530a8219a6204192bae1f0c75aa27276a3e
Muhammad Khoirul Anam <mkhoirulanam920@gmail.com>
2026-07-16 01:45:46 +0700

    feat(sections): implement hero section layout

---
+++ src/App.vue
@@ -0,0 +1,9 @@
+<script setup lang="ts">
+import HeroSection from './sections/HeroSection.vue'
+</script>
+
+<template>
+  <main class="min-h-screen bg-zinc-900 w-full overflow-x-hidden">
+    <HeroSection />
+  </main>
+</template>
